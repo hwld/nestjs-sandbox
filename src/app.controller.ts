a2bd7f0f75e3dc215eed587cebdca767793bcde3
hwld <0701hw@gmail.com>
2023-10-24 13:55:46 +0900

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,11 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    throw new HttpException(
+      { code: 1, message: 'a' },
+      HttpStatus.INTERNAL_SERVER_ERROR,
+      { cause: new Error('eee') },
+    );
     return this.appService.getHello();
   }
 }
---
+++ src/http-exception/http-exception.filter.ts
@@ -0,0 +1,8 @@
+import { Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException) {
+    console.error(exception);
+  }
+}
